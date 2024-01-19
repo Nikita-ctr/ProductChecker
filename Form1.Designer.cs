8da756c09754fa823331c097e200bd4cb65048fe
Nikita Khmileuski <xmil.nik@gmail.com>
2024-01-19 19:20:10 +0300

    rebuild

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,8 @@
-﻿namespace Solution
+﻿using System.Windows.Forms;
+
+namespace Solution
 {
-    partial class Form1
+    partial class ParseProductCode
     {
         /// <summary>
         /// Required designer variable.
@@ -19,53 +21,46 @@
             }
             base.Dispose(disposing);
         }
-
+        private ErrorProvider errorProvider;
         #region Windows Form Designer generated code
 
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(273, 107);
+            this.errorProvider.SetIconPadding(this.textBox1, 2);
+            this.textBox1.Location = new System.Drawing.Point(197, 116);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(280, 20);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(263, 81);
+            this.label1.Location = new System.Drawing.Point(270, 81);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.Size = new System.Drawing.Size(108, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Введите количество кг";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(272, 141);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Считать товар";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.Text = "Введите код товара";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(251, 212);
+            this.label2.Location = new System.Drawing.Point(253, 165);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(148, 13);
             this.label2.TabIndex = 3;
@@ -74,16 +69,16 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(486, 81);
+            this.label3.Location = new System.Drawing.Point(589, 47);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.Size = new System.Drawing.Size(101, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Оставшийся вес";
+            this.label3.Text = "Сумма килограмм";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(520, 110);
+            this.label4.Location = new System.Drawing.Point(632, 81);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(13, 13);
             this.label4.TabIndex = 5;
@@ -92,22 +87,22 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(263, 9);
+            this.label5.Location = new System.Drawing.Point(273, 10);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(145, 13);
+            this.label5.Size = new System.Drawing.Size(102, 13);
             this.label5.TabIndex = 6;
-            this.label5.Text = "Максимальный вес товара";
+            this.label5.Text = "Лимит на вес в кг.";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(275, 40);
+            this.textBox2.Location = new System.Drawing.Point(256, 40);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(145, 20);
             this.textBox2.TabIndex = 7;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(476, 141);
+            this.button2.Location = new System.Drawing.Point(589, 116);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 23);
             this.button2.TabIndex = 8;
@@ -115,33 +110,37 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button4
+            // errorProvider
             // 
-            this.button4.Location = new System.Drawing.Point(245, 177);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(163, 23);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "Запустить авто-проверку";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.errorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider.ContainerControl = this;
             // 
-            // Form1
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(173, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0";
+            // 
+            // ParseProductCode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
+            this.Name = "ParseProductCode";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,14 +150,13 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button4;
+        private Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,155 +5,328 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace Solution {
-    public partial class Form1 : Form {
-
-        private static Random random = new Random();
+namespace Solution
+{
+    public partial class ParseProductCode : Form
+    {
+        private double totalWeight = 0;
         private double currentWeight = 0.0;
-        private List<string> codes = new List<string>(); 
-        private double remainingWeight;
-        private double targetWeight = 0.0;
         private ListBox listBoxCodes = new ListBox();
-        private static int maxWeight = 0;
-        private bool isAutoCheckRunning = false;
-        public Form1() { 
+        private Timer timer = new Timer();
+        private int absoluteTime = 2;
+        private string currentCodeFormat = @"^\(01\)\d+\(3103\)\d+\(11\)\d+\(10\)\d+\(21\)\d+$";
+        private int remainingTime = 0;
+        private System.Windows.Forms.Button buttonSettings;
+        private ContextMenuStrip contextMenuStripSettings; 
+        private ToolStripMenuItem toolStripMenuItemTime; 
+        private TimeSettingsForm timeSettingsForm;
+        private ToolStripMenuItem toolStripMenuItemTimeSettings;
+        private ToolStripMenuItem toolStripMenuItemCodeFormatSettings;
+
+        public ParseProductCode()
+        {
             InitializeComponent();
             InitializeListBox();
+
+            buttonSettings = new System.Windows.Forms.Button();
+            buttonSettings.Location = new Point(20, 20);
+            buttonSettings.Text = "Настройки";
+            buttonSettings.Click += ButtonSettings_Click;
+            Controls.Add(buttonSettings);
+
+            contextMenuStripSettings = new ContextMenuStrip();
+            toolStripMenuItemTimeSettings = new ToolStripMenuItem("Настроить время");
+            toolStripMenuItemTimeSettings.Click += ToolStripMenuItemTime_Click;
+            contextMenuStripSettings.Items.Add(toolStripMenuItemTimeSettings);
+
+            toolStripMenuItemCodeFormatSettings = new ToolStripMenuItem("Настроить формат кода");
+            toolStripMenuItemCodeFormatSettings.Click += ToolStripMenuItemCodeFormat_Click;
+            contextMenuStripSettings.Items.Add(toolStripMenuItemCodeFormatSettings);
+
+            remainingTime = 2;
+
+            timer.Interval = 1000;
+            timer.Tick += (s, e) =>
+            {
+                remainingTime--;
+                label6.Text = remainingTime.ToString();
+
+                if (remainingTime <= 0)
+                {
+                    Timer_Tick(s, e);
+                }
+            };
+
+            textBox1.TextChanged += TextBox1_TextChanged;
         }
 
-        private void InitializeListBox(){
+        private void ToolStripMenuItemCodeFormat_Click(object sender, EventArgs e)
+        {
+            using (var codeFormatSettingsForm = new CodeFormatSettingsForm(GetCodeFormat()))
+            {
+                if (codeFormatSettingsForm.ShowDialog() == DialogResult.OK)
+                {
+                    SetCodeFormat(codeFormatSettingsForm.CodeFormat);
+                }
+            }
+        }
+        private string GetCodeFormat()
+        {
+            return currentCodeFormat;
+        }
+
+        private void SetCodeFormat(string codeFormat)
+        {
+            currentCodeFormat = codeFormat;
+        }
+
+        private void ButtonFormatSettings_Click(object sender, EventArgs e)
+        {
+            using (var codeFormatSettingsForm = new CodeFormatSettingsForm(GetCodeFormat()))
+            {
+                if (codeFormatSettingsForm.ShowDialog() == DialogResult.OK)
+                {
+                    SetCodeFormat(codeFormatSettingsForm.CodeFormat); 
+                }
+            }
+        }
+
+        private void ButtonSettings_Click(object sender, EventArgs e)
+        {
+            contextMenuStripSettings.Show(buttonSettings, new Point(0, buttonSettings.Height));
+        }
+
+        private void ToolStripMenuItemTime_Click(object sender, EventArgs e)
+        {
+            using (var timeSettingsForm = new TimeSettingsForm())
+            {
+                if (timeSettingsForm.ShowDialog() == DialogResult.OK)
+                {
+                   
+                    int timeInSeconds = timeSettingsForm.TimeInSeconds;
+                   
+                    absoluteTime = timeInSeconds;
+                    label6.Text = remainingTime.ToString();
+
+                    if (timer.Enabled)
+                    {
+                        timer.Stop();
+                        timer.Start();
+                    }
+                }
+            }
+        }
+
+        private void InitializeListBox()
+        {
             listBoxCodes.Location = new System.Drawing.Point(220, 240);
             listBoxCodes.Size = new System.Drawing.Size(300, 100);
             Controls.Add(listBoxCodes);
         }
 
-        private void SetControlsEnabled(bool enabled)
+        private void UpdateTotalWeight()
         {
-            textBox1.Enabled = enabled;
-            textBox2.Enabled = enabled;
- 
-        }
-
-        public static string GenerateRandomCode() {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("(01)54610268019390(3103)");
-            Console.WriteLine(maxWeight);
-            int randomValue = random.Next(1, (int)maxWeight);
-            sb.Append(randomValue.ToString());
-            sb.Append("(11)230630(10)3999(21)0001");
-            return sb.ToString();
-        }
-
-        private  void button1_Click(object sender, EventArgs e) {
-            if (double.TryParse(textBox1.Text, out targetWeight)){
-                int.TryParse(textBox2.Text, out maxWeight);
-                currentWeight = 0.0;
-                SetControlsEnabled(false);
-                for (int i = 0; i < 1; i++){
-                    string randomCode = GenerateRandomCode();
-                    listBoxCodes.Items.Add(randomCode);
-                    codes.Add(randomCode);
-                }
-                remainingWeight = targetWeight - currentWeight;
-                label4.Text = remainingWeight.ToString();
-
-                ProcessProductCodes(codes);
-            }
-        }
-        private async Task ProcessProductCodes(List<string> productCodes) {
-            currentWeight = 0.0;
-            foreach (string code in productCodes) {
-                if (code.StartsWith("(01)") && code.Length >= 15) {
-                    int startIndex = code.IndexOf("(3103)") + 6;
-                    int endIndex = code.IndexOf("(11)", startIndex);
-
-                    if (endIndex != -1 && int.TryParse(code.Substring(startIndex, endIndex - startIndex), out int weight)) {
-                        Console.WriteLine(weight);
-                        currentWeight += weight;
-                    }
-                }
-            }
-            remainingWeight = targetWeight - currentWeight;
-            label4.Text = remainingWeight.ToString();
-
-            if (currentWeight >= targetWeight){
-                MessageBox.Show("Достигнут заданный вес продукта!", 
-                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                isAutoCheckRunning = false;
-                button4.Text = "Запустить авто-проверку";
-                return;
-            }
-
-            if (remainingWeight - maxWeight <= 0) {
-                MessageBox.Show("Возможно переполнение, так как максимальный вес продукта (" + maxWeight + ") " +
-                    "больше или равен оставшемуся весу (" + remainingWeight + ").");
-                isAutoCheckRunning = false;
-                button4.Text = "Запустить авто-проверку";
-            }
-        }
-
-        private void ResetValues() {
-            currentWeight = 0.0;
-            textBox1.Clear();
-            textBox2.Clear();
-            remainingWeight = targetWeight;
-            codes.Clear();
-            listBoxCodes.Items.Clear();
-            label4.Text = "0";
-        }
-
-        private void button2_Click(object sender, EventArgs e) {
-            SetControlsEnabled(true);
-            ResetValues();
+            label4.Text = totalWeight.ToString();
         }
 
 
-
-
-        private void button4_Click(object sender, EventArgs e) {
-            SetControlsEnabled(false);
-            if (isAutoCheckRunning)
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            string code = textBox1.Text;
+            double weight = GetProductWeight(code);
+            if (weight > 0)
             {
-              
-                isAutoCheckRunning = false;
-                button4.Text = "Запустить авто-проверку"; 
+                if (string.IsNullOrEmpty(textBox2.Text))
+                {
+                    MessageBox.Show("Поле максимального веса товаров не может быть пустым.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                double newTotalWeight = totalWeight + weight;
+                if (newTotalWeight > Convert.ToDouble(textBox2.Text))
+                {
+                    MessageBox.Show("Превышен максимальный вес товаров!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    listBoxCodes.Items.Add(code);
+                    totalWeight = newTotalWeight;
+                    UpdateTotalWeight();
+                }
+            }
+            textBox1.Clear();
+            textBox1.BackColor = SystemColors.Window;
+            errorProvider.SetError(textBox1, "");
+            remainingTime = absoluteTime;
+            label6.Text = remainingTime.ToString();
+        }
+
+       private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+        string code = textBox1.Text;
+        bool isValid = IsProductCodeValid(code);
+
+            if (isValid)
+            {
+                textBox1.BackColor = Color.LightGreen;
+                errorProvider.SetError(textBox1, "");
+                timer.Stop();
+                remainingTime = absoluteTime;
+                label6.Text = remainingTime.ToString();
+                timer.Start();
             }
             else
             {
-           
-                isAutoCheckRunning = true;
-                button4.Text = "Остановить авто-проверку"; 
-                RunAutoCheck();
+                textBox1.BackColor = SystemColors.Window;
+                errorProvider.SetError(textBox1, "Неверный формат кода товара");
+                timer.Stop();
+                remainingTime = 0;
+                label6.Text = remainingTime.ToString();
             }
         }
 
-        private async void RunAutoCheck()
+        private bool IsProductCodeValid(string code)
+        {
+            string pattern = @"^\(01\)\d+\(3103\)\d+\(11\)\d+\(10\)\d+\(21\)\d+$";
+            return Regex.IsMatch(code, pattern);
+        }
+
+        private double GetProductWeight(string code)
         {
             currentWeight = 0.0;
-    
-            if (double.TryParse(textBox1.Text, out targetWeight))
+            int weight = 0;
+            if (code.StartsWith("(01)") && code.Length >= 15)
             {
-                int.TryParse(textBox2.Text, out maxWeight);
+                int startIndex = code.IndexOf("(3103)") + 6;
+                int endIndex = code.IndexOf("(11)", startIndex);
 
-                while (isAutoCheckRunning && currentWeight < targetWeight)
+                if (endIndex != -1 && int.TryParse(code.Substring(startIndex, endIndex - startIndex), out weight))
                 {
-                    string randomCode = GenerateRandomCode();
-                    listBoxCodes.Items.Add(randomCode);
-                    codes.Add(randomCode);
-
-                    remainingWeight = targetWeight - currentWeight;
-                    label4.Text = remainingWeight.ToString();
-
-                    await Task.Delay(1000);
-
-                    await ProcessProductCodes(codes);
+                    Console.WriteLine(weight);
+                    currentWeight += weight;
                 }
             }
+            return weight;
+        }
+
+        private void ResetValues()
+        {
+            currentWeight = 0.0;
+            textBox1.Clear();
+            textBox2.Clear();
+            listBoxCodes.Items.Clear();
+            totalWeight = 0.0;
+            UpdateTotalWeight();
+            textBox1.BackColor = SystemColors.Window;
+            errorProvider.SetError(textBox1, "");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResetValues();
         }
 
     }
-}
+    public class TimeSettingsForm : Form
+    {
+        private NumericUpDown numericUpDownTime;
+        private System.Windows.Forms.Button buttonOK;
 
+        public int TimeInSeconds => (int)numericUpDownTime.Value;
+
+        public TimeSettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            numericUpDownTime = new NumericUpDown();
+            buttonOK = new System.Windows.Forms.Button();
+
+            numericUpDownTime.Location = new Point(20, 20);
+            numericUpDownTime.Width = 100;
+            numericUpDownTime.Minimum = 1;
+            numericUpDownTime.Maximum = 60;
+
+            buttonOK.Location = new Point(20, 50);
+            buttonOK.Text = "OK";
+            buttonOK.DialogResult = DialogResult.OK;
+            buttonOK.Click += ButtonOK_Click;
+
+            Controls.Add(numericUpDownTime);
+            Controls.Add(buttonOK);
+
+            Text = "Настройки времени";
+            Width = 200;
+            Height = 120;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            StartPosition = FormStartPosition.CenterParent;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            AcceptButton = buttonOK;
+            CancelButton = buttonOK;
+        }
+
+        private void ButtonOK_Click(object sender, EventArgs e)
+        {
+        
+            Close();
+        }
+    }
+
+    public class CodeFormatSettingsForm : Form
+    {
+        private System.Windows.Forms.TextBox textBoxFormat;
+        private System.Windows.Forms.Button buttonOK;
+
+   
+
+        public string CodeFormat { get; private set; }
+
+        public CodeFormatSettingsForm(string codeFormat)
+        {
+            CodeFormat = codeFormat;
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            textBoxFormat = new System.Windows.Forms.TextBox();
+            buttonOK = new System.Windows.Forms.Button();
+
+            textBoxFormat.Location = new Point(20, 20);
+            textBoxFormat.Width = 200;
+
+            buttonOK.Location = new Point(20, 50);
+            buttonOK.Text = "OK";
+            buttonOK.DialogResult = DialogResult.OK;
+            buttonOK.Click += ButtonOK_Click;
+
+            Controls.Add(textBoxFormat);
+            Controls.Add(buttonOK);
+
+            Text = "Настройка формата кода";
+            Width = 250;
+            Height = 120;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            StartPosition = FormStartPosition.CenterParent;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            AcceptButton = buttonOK;
+            CancelButton = buttonOK;
+        }
+
+        private void ButtonOK_Click(object sender, EventArgs e)
+        {
+            CodeFormat = textBoxFormat.Text;
+            Close();
+        }
+    }
+}
